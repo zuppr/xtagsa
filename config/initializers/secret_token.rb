b734f46f886950a56448d0dae2687068ff757140
Zuppr <zuppr@yahoo.co.uk>
2010-10-18 10:32:00 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xtagsa::Application.config.secret_token = '43d53dd037dbf542bbc51498ad3c1c7742489d9f26d87214a0c490fdfd84138e151bdb6a0dac28c03d1e0cbbe63c7539054b24c2172313a102bfad9727c66624'
